c36eeeb6a4adecf9a1333ad81ac80cdf3dcab072
kaballah <kabzkabala4@gmail.com>
2022-09-01 19:53:06 +0300

    Included a navigation bar as well

--- js/script.js
+++ js/script.js
@@ -81,4 +81,31 @@ function myFunction() {
     document.getElementById('myBar').style.width = scrolled + "%";
 }
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+// Dropdown nav-bar
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+  
 
